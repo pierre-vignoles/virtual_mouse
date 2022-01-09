a73dc9e556b4ec924a251e571dd8c0a3307f7398
pierre-vignoles <pierre.94800@gmail.com>
2022-01-09 15:37:23 +0100

    First commit

---
+++ config_file.py
@@ -0,0 +1,7 @@
+w_cam: int = 1280
+h_cam: int = 720
+framR: int = 200
+smoothening: int = 5
+previous_location_x, previous_location_y = 0, 0
+current_location_x, current_location_y = 0, 0
+previous_mode = 'None'
